cb0eb6d131be8e9a0953dc7e62151e0b60303576
VanisreeThazhedamVeedu <t.vanisri@yahoo.in>
2019-01-21 16:47:08 +0530

    Add files via upload

--- js/my.js
+++ js/my.js
@@ -5,30 +5,21 @@
 
     function onDeviceReady() {
         $(document).on('click', '#go', function(){
-    var website =  $("#text-basic").val();
-    alert(website);
+    var ip =  $("#text-basic").val();
+    alert(ip);
      window.open = cordova.InAppBrowser.open;
        //  var ref = window.open("http://www."+website, '_blank', 'location=yes');
-       var ref = window.open("http://192.168.0.130:3090/desktop-authenticate2?reseller_id=305&store_id=144", '_blank', 'location=yes');
+      // var ref = window.open("http://192.168.0.130:3090/desktop-authenticate2?reseller_id=305&store_id=144", '_blank', 'location=yes');
+       var ref = window.open("http://" + ip + ":3090/desktop-authenticate2?reseller_id=305&store_id=144", '_blank', 'location=yes');
          ref.addEventListener('loadstart', function(event) { 
              //alert('start: ' + event.url);
              });
          ref.addEventListener('loadstop', function(event) { 
-            window.open('http://192.168.0.130:3090/home', '_blank', 'location=yes');
+            window.open('"http://" + ip + ":3090/home', '_blank', 'location=yes');
             // alert('stop: ' + event.url); 
         });
          ref.addEventListener('loaderror', function(event) { /*alert('error: ' + event.message);*/ });
          ref.addEventListener('exit', function(event) { /*alert(event.type);*/ });
 });
 
-openURL = function(){
-    var website =  $("#text-basic").val();
-    alert(website);
-     window.open = cordova.InAppBrowser.open;
-         var ref = window.open("http://www."+website, '_blank', 'location=yes');
-         ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
-         ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
-         ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
-         ref.addEventListener('exit', function(event) { alert(event.type); });
-}
     }
