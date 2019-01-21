79dfb14866ec7f89c51d29450e48197b75a855d3
VanisreeThazhedamVeedu <t.vanisri@yahoo.in>
2019-01-21 15:16:18 +0530

    Add files via upload

--- js/my.js
+++ js/my.js
@@ -9,12 +9,12 @@
     alert(website);
      window.open = cordova.InAppBrowser.open;
        //  var ref = window.open("http://www."+website, '_blank', 'location=yes');
-       var ref = window.open("http://localhost:3090/desktop-authenticate2?reseller_id=305&store_id=144", '_blank', 'location=yes');
+       var ref = window.open("http://192.168.0.130:3090/desktop-authenticate2?reseller_id=305&store_id=144", '_blank', 'location=yes');
          ref.addEventListener('loadstart', function(event) { 
              //alert('start: ' + event.url);
              });
          ref.addEventListener('loadstop', function(event) { 
-            window.open('http://apache.org', '_blank', 'location=yes');
+            window.open('http://192.168.0.130:3090/home', '_blank', 'location=yes');
             // alert('stop: ' + event.url); 
         });
          ref.addEventListener('loaderror', function(event) { /*alert('error: ' + event.message);*/ });
