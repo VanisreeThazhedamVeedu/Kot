97dff9b8c11128d232abcc258b1c0926c9bccdf3
VanisreeThazhedamVeedu <t.vanisri@yahoo.in>
2019-02-13 15:44:23 +0530

    Add files via upload

--- js/my.js
+++ js/my.js
@@ -12,13 +12,13 @@
      window.open = cordova.InAppBrowser.open;
        //  var ref = window.open("http://www."+website, '_blank', 'location=yes');
       // var ref = window.open("http://192.168.0.130:3090/desktop-authenticate2?reseller_id=305&store_id=144", '_blank', 'location=yes');
-       var ref = window.open("http://" + ip + ":3090/desktop-authenticate2?reseller_id=" + reseller_id + "store_id=" + store_id, '_blank', 'location=yes');
+       var ref = window.open("http://" + ip + ":3090/desktop-authenticate2?reseller_id=451&store_id=364", '_blank', 'location=yes');
          ref.addEventListener('loadstart', function(event) { 
              //alert('start: ' + event.url);
              });
          ref.addEventListener('loadstop', function(event) { 
             window.open("http://" + ip + ":3090/kitchen-display", "_blank", "location=yes");
-           // ref.insertCSS({file: "../css/kot.css"});
+            ref.insertCSS({file: "../css/kot.css"});
             // alert('stop: ' + event.url); 
         });
          ref.addEventListener('loaderror', function(event) { /*alert('error: ' + event.message);*/ });
